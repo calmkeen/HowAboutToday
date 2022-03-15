319aef010104aa270934dcf14ab20291d7c6dfa2
calmkeen <iyunseong159@gmail.com>
2022-03-15 15:07:49 +0900

    Initial Commit

---
+++ HowAboutToday/HowAboutTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowAboutTodayApp.swift
+//  HowAboutToday
+//
+//  Created by calmkeen on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct HowAboutTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
